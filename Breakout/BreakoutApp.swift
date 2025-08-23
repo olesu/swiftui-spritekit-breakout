b220d82a6e2a7f96b2792bcdbeba985ef325ec0e
Ole Kristian Sunde <ole.sunde@gmail.com>
2025-08-23 07:49:57 +0200

    Initial Commit

---
+++ Breakout/BreakoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakoutApp.swift
+//  Breakout
+//
+//  Created by Ole Kristian Sunde on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreakoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Breakout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Breakout
+//
+//  Created by Ole Kristian Sunde on 23/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
