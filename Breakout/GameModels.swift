afe903ad4ac82f6e8fc44e03184912d830b31523
Ole Kristian Sunde <ole.sunde@gmail.com>
2025-08-27 20:51:49 +0200

    clamp paddle inside game area

--- Breakout/GameModels.swift
+++ Breakout/GameModels.swift
@@ -6,10 +6,25 @@ struct Brick: Identifiable {
 }
 
 struct Paddle {
-    var position: CGPoint = CGPoint(x: 200, y: 50)
-    var size: CGSize = CGSize(width: 80, height: 16)
+    var position: CGPoint
+    var size: CGSize
+    let minX: CGFloat
+    let maxX: CGFloat
+
+    init(
+        position: CGPoint = CGPoint(x: 200, y: 50),
+        size: CGSize = CGSize(width: 80, height: 16),
+        minX: CGFloat = 0,
+        maxX: CGFloat = 400
+    ) {
+        self.position = position
+        self.size = size
+        self.minX = minX
+        self.maxX = maxX
+    }
 
     mutating func move(to x: CGFloat) {
-        position = CGPoint(x: x, y: position.y)
+        let clampedX = min(max(x, minX), maxX)
+        position = CGPoint(x: clampedX, y: position.y)
     }
 }
--- BreakoutTests/PaddleTests.swift
+++ BreakoutTests/PaddleTests.swift
@@ -14,4 +14,16 @@ struct PaddleTests {
         paddle.move(to: 120)
         #expect(paddle.position == CGPoint(x: 120, y: 50))
     }
+
+    @Test func paddleCannotMoveLeftOutsideGameArea() {
+        var paddle = Paddle()
+        paddle.move(to: -100)
+        #expect(paddle.position.x >= 0)
+    }
+
+    @Test func paddleCannotMoveRightOutsideGameArea() {
+        var paddle = Paddle()
+        paddle.move(to: 500)
+        #expect(paddle.position.x <= 400)
+    }
 }
