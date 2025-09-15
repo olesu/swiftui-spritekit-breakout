b394f372fd173869b9c71bc1aafa384220f18e8b
Ole Kristian Sunde <ole.sunde@gmail.com>
2025-09-15 18:13:24 +0200

    makes the ball move

--- Breakout/GameView.swift
+++ Breakout/GameView.swift
@@ -40,6 +40,21 @@ class BallSprite: SKSpriteNode {
         super.init(texture: nil, color: .white, size: ballSize)
         self.name = NodeNames.ball.rawValue
         self.position = position
+        
+        // Add physics body
+        self.physicsBody = SKPhysicsBody(rectangleOf: ballSize)
+        self.physicsBody?.isDynamic = true
+        self.physicsBody?.categoryBitMask = 4
+        self.physicsBody?.contactTestBitMask = 1 | 2
+        self.physicsBody?.collisionBitMask = 1
+        self.physicsBody?.restitution = 1.0
+        self.physicsBody?.friction = 0
+        self.physicsBody?.linearDamping = 0
+        self.physicsBody?.angularDamping = 0
+        self.physicsBody?.affectedByGravity = false
+        
+        // Set initial velocity (diagonal up and right)
+        self.physicsBody?.velocity = CGVector(dx: 200, dy: 300)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -296,6 +311,9 @@ class BreakoutScene: SKScene {
     override func didMove(to view: SKView) {
         view.showsPhysics = true
         view.showsFPS = true
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        
         sprites.values.forEach { addChild($0) }
     }
 }
