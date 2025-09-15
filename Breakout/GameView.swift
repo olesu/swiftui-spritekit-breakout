2b030fb80bada15fa70fadfe40662ee2561044de
Ole Kristian Sunde <ole.sunde@gmail.com>
2025-09-15 17:26:44 +0200

    use data instead of calculations to define the brick layout

--- Breakout/GameView.swift
+++ Breakout/GameView.swift
@@ -31,9 +31,14 @@ class BallSprite: SKSpriteNode {
     
 }
 
+struct BrickData {
+    let position: CGPoint
+    let color: NSColor
+}
+
 class BrickSprite: SKSpriteNode {
     init(position: CGPoint, color: NSColor) {
-        let brickSize = CGSize(width: 30, height: 12)
+        let brickSize = CGSize(width: 22, height: 10)
         super.init(texture: nil, color: color, size: brickSize)
         self.position = position
     }
@@ -44,6 +49,129 @@ class BrickSprite: SKSpriteNode {
 }
 
 class ClassicBricksLayout: SKNode {
+    let brickLayout: [BrickData] = [
+        // Red row (top)
+        BrickData(position: CGPoint(x: 11, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 34, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 57, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 80, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 103, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 126, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 149, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 172, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 195, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 218, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 241, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 264, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 287, y: 420), color: .red),
+        BrickData(position: CGPoint(x: 310, y: 420), color: .red),
+        // Red row 2
+        BrickData(position: CGPoint(x: 11, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 34, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 57, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 80, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 103, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 126, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 149, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 172, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 195, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 218, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 241, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 264, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 287, y: 408), color: .red),
+        BrickData(position: CGPoint(x: 310, y: 408), color: .red),
+        // Orange row
+        BrickData(position: CGPoint(x: 11, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 34, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 57, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 80, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 103, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 126, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 149, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 172, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 195, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 218, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 241, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 264, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 287, y: 396), color: .orange),
+        BrickData(position: CGPoint(x: 310, y: 396), color: .orange),
+        // Orange row 2
+        BrickData(position: CGPoint(x: 11, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 34, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 57, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 80, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 103, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 126, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 149, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 172, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 195, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 218, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 241, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 264, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 287, y: 384), color: .orange),
+        BrickData(position: CGPoint(x: 310, y: 384), color: .orange),
+        // Yellow row
+        BrickData(position: CGPoint(x: 11, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 34, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 57, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 80, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 103, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 126, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 149, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 172, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 195, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 218, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 241, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 264, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 287, y: 372), color: .yellow),
+        BrickData(position: CGPoint(x: 310, y: 372), color: .yellow),
+        // Yellow row 2
+        BrickData(position: CGPoint(x: 11, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 34, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 57, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 80, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 103, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 126, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 149, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 172, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 195, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 218, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 241, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 264, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 287, y: 360), color: .yellow),
+        BrickData(position: CGPoint(x: 310, y: 360), color: .yellow),
+        // Green row
+        BrickData(position: CGPoint(x: 11, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 34, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 57, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 80, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 103, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 126, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 149, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 172, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 195, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 218, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 241, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 264, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 287, y: 348), color: .green),
+        BrickData(position: CGPoint(x: 310, y: 348), color: .green),
+        // Green row 2
+        BrickData(position: CGPoint(x: 11, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 34, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 57, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 80, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 103, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 126, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 149, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 172, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 195, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 218, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 241, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 264, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 287, y: 336), color: .green),
+        BrickData(position: CGPoint(x: 310, y: 336), color: .green)
+    ]
+    
     override init() {
         super.init()
         setupBricks()
@@ -54,22 +182,9 @@ class ClassicBricksLayout: SKNode {
     }
     
     private func setupBricks() {
-        let brickColors: [NSColor] = [.red, .orange, .yellow, .green, .blue, .purple]
-        let bricksPerRow = 10
-        let brickWidth: CGFloat = 30
-        let brickHeight: CGFloat = 12
-        let spacing: CGFloat = 2
-        let totalWidth = CGFloat(bricksPerRow) * brickWidth + CGFloat(bricksPerRow - 1) * spacing
-        let startX = (320 - totalWidth) / 2 + brickWidth / 2
-        
-        for row in 0..<6 {
-            let color = brickColors[row]
-            for col in 0..<bricksPerRow {
-                let x = startX + CGFloat(col) * (brickWidth + spacing)
-                let y = CGFloat(row) * (brickHeight + spacing) + 350
-                let brick = BrickSprite(position: CGPoint(x: x, y: y), color: color)
-                addChild(brick)
-            }
+        brickLayout.forEach { brickData in
+            let brick = BrickSprite(position: brickData.position, color: brickData.color)
+            addChild(brick)
         }
     }
 }
