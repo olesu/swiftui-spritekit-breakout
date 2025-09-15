f836f2953f681aa2d8975cc0cd8780d71a8ca70f
Ole Kristian Sunde <ole.sunde@gmail.com>
2025-09-15 16:43:41 +0200

    sets up the game view and scene size

--- Breakout/GameView.swift
+++ Breakout/GameView.swift
@@ -38,18 +38,24 @@ class GutterNode: SKNode {
 }
 
 class BreakoutScene: SKScene {
+    let gameSize = CGSize(width: 320, height: 480)
+    
     override func didMove(to view: SKView) {
-        
+        size = gameSize
     }
 }
 
 struct GameView: View {
     var breakoutScene: SKScene {
-        BreakoutScene()
+        let scene = BreakoutScene()
+        scene.scaleMode = .aspectFit
+        scene.backgroundColor = .black
+        return scene
     }
     
     var body: some View {
         SpriteView(scene: breakoutScene)
+            .frame(width: 320, height: 480)
     }
 }
 
