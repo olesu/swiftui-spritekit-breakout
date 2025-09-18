15942c434fee5707276e758c58c733f6266e3aaa
Ole Kristian Sunde <ole.sunde@gmail.com>
2025-09-18 19:39:35 +0200

    remove old code

--- Breakout/Views/ContentView.swift
+++ Breakout/Views/ContentView.swift
@@ -8,48 +8,9 @@
 import SwiftUI
 import SpriteKit
 
-class GameController: ObservableObject {
-    let gameService: GameService
-    let scene: GameScene
-    
-    init() {
-        let bricks = [
-            Brick(id: UUID()),
-            Brick(id: UUID())
-        ]
-        let service = AGameService(remainingLives: 3, bricks: bricks)
-        self.gameService = service
-        self.scene = GameScene(gameService: service, size: CGSize(width: 400, height: 600))
-    }
-    
-    func hitBrick() {
-        if let brick = gameService.bricks.first {
-            scene.brickHit(brick)
-        }
-    }
-    
-    func loseBall() {
-        scene.ballHitBottom()
-    }
-}
-
 struct ContentView: View {
-    @StateObject private var gameController = GameController()
-
     var body: some View {
-        VStack {
-            SpriteView(scene: gameController.scene)
-                .frame(width: 400, height: 600)
-                .ignoresSafeArea()
-            HStack {
-                Button("Hit Brick") {
-                    gameController.hitBrick()
-                }
-                Button("Lose Ball") {
-                    gameController.loseBall()
-                }
-            }
-        }
+        GameView()
     }
 }
 
