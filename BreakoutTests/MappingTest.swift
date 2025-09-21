327cfc5d74b6279acd92c194db239e4942c127a1
Ole Kristian Sunde <ole.sunde@gmail.com>
2025-09-21 10:15:58 +0200

    adds a little learning test

---
+++ BreakoutTests/MappingTest.swift
@@ -0,0 +1,22 @@
+//
+//  MappingTest.swift
+//  BreakoutTests
+//
+//  Created by Ole Kristian Sunde on 21/09/2025.
+//
+
+import Testing
+
+struct MappingTest {
+
+    @Test func flattenAnArray() async throws {
+        let ints: [Int] = doFlatten(ints: [1, 2], inject: [3, 4, 5])
+        
+        #expect(ints == [1, 2, 3, 4, 5])
+    }
+    
+    func doFlatten(ints: [Int], inject: [Int]) -> [Int] {
+        return ints + inject
+    }
+
+}
