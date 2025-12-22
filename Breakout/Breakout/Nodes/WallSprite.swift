99af464dd50fdaf2d7f5afa213603b1623cefc93
Ole Kristian Sunde <ole.sunde@gmail.com>
2025-12-22 18:18:24 +0100

    makes the walls visible, will polish further, some day...

--- Breakout/Breakout/Nodes/WallSprite.swift
+++ Breakout/Breakout/Nodes/WallSprite.swift
@@ -1,13 +1,45 @@
+import AppKit
 import SpriteKit
 
-internal final class WallSprite: SKSpriteNode {
-    internal init(position: CGPoint, size: CGSize) {
-        super.init(texture: nil, color: .clear, size: size)
+final class WallSprite: SKSpriteNode {
+
+    init(position: CGPoint, size: CGSize) {
+        let texture = SKTexture(
+            image: WallTextureFactory.diagonalStripes()
+        )
+
+        super.init(texture: texture, color: .clear, size: size)
+
         self.position = position
-        self.physicsBody = WallPhysicsBodyConfigurer.init(size: size).physicsBody
+        self.alpha = 0.35
+        self.physicsBody = WallPhysicsBodyConfigurer(size: size).physicsBody
     }
 
-    required init?(coder aDecoder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+enum WallTextureFactory {
+
+    static func diagonalStripes(
+        size: CGSize = CGSize(width: 16, height: 16)
+    ) -> NSImage {
+        let image = NSImage(size: size)
+        image.lockFocus()
+
+        NSColor.white.withAlphaComponent(0.4).setFill()
+        NSBezierPath(rect: NSRect(origin: .zero, size: size)).fill()
+
+        NSColor.gray.withAlphaComponent(0.6).setStroke()
+
+        let path = NSBezierPath()
+        path.lineWidth = 2
+        path.move(to: CGPoint(x: 0, y: size.height))
+        path.line(to: CGPoint(x: size.width, y: 0))
+        path.stroke()
+
+        image.unlockFocus()
+        return image
     }
 }
